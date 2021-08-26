a327f5dd397a881bdb4d477b84e227692f851c35
Swastik Mukherjee <swastik.reach@gmail.com>
2021-08-26 13:18:36 +0545

    Display all elemment has been created

--- linked_list/simple_linked_list.py
+++ linked_list/simple_linked_list.py
@@ -16,11 +16,18 @@ def search(head, key):
             index = index + 1
 
 
+def display(head):
+    while(head):
+        print(head.key)
+        head = head.next
+
+
 # driver code
 head = Node(10)
 head.next = Node(20)
 head.next.next = Node(30)
 head.next.next.next = Node(40)
-x = 20
 
-print(search(head, 40))
+
+#print(search(head, 40))
+display(head)
